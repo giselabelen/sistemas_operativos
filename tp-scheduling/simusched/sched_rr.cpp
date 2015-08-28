8c7b0491e80e47b04a10b411c412758b5967b51e
Minga992 <minga_titere@hotmail.com>
2015-08-28 17:20:23 -0300

    terminando round robin, falta compilar, funcion auxiliar y el pequeño detalle de si funciona...

--- tp-scheduling/simusched/sched_rr.cpp
+++ tp-scheduling/simusched/sched_rr.cpp
@@ -29,17 +29,20 @@ SchedRR::~SchedRR() {
 
 void SchedRR::load(int pid) {
 	
+	// cargo los datos de la tarea que ingresa
 	tarea nueva;
 	nueva.pid = pid;
-	nueva.cpu = -1;
+	nueva.cpu = -1;	// sin cpu asignado
 	nueva.bloqueado = false;
-	nueva.quantum = 0;
+	nueva.quantum = 0;	// y sin quantum
 	// hasta ahi inicie el elemento
 	
 	int i;
 	
+	// busco si hay un cpu libre para correr esta tarea
 	for(i = 0; i < cant_cpu; i++)
 	{
+		// si lo encuentro, lo uso
 		if(actual_x_cpu[i] == NULL)
 		{
 			nueva.cpu = i;
@@ -49,6 +52,7 @@ void SchedRR::load(int pid) {
 		}
 	}
 	
+	// si no, la pongo en espera
 	if(i == cant_cpu)
 	{
 		proc_esperando.push_back(nueva);
@@ -57,6 +61,7 @@ void SchedRR::load(int pid) {
 
 void SchedRR::unblock(int pid) {
 	
+	// buscar la tarea correspondiente en las que están esperando y desbloquearla
 	for(list<tarea>::iterator it = proc_esperando.begin(); it != proc_esperando.end(); it++)
 	{
 		if(it->pid == pid)
@@ -72,27 +77,54 @@ int SchedRR::tick(int cpu, const enum Motivo m) {
 	switch(m)
 	{
 		case TICK:
-			
+			actual_x_cpu[cpu].quantum--;
+			if(actual_x_cpu[cpu] > 0)
+			{
+				return actual_x_cpu[cpu].pid;
+			}else{
+				if(proc_esperando.empty())
+				{
+					actual_x_cpu[cpu].quantum = quantum_x_cpu[cpu];
+					return actual_x_cpu[cpu].pid;
+				}else{
+					// HACER LO MISMO QUE EN EXIT - segunda parte
+					// (CONTEXT_SWITCH)
+				}
+			}
+			break;
 		
 		case BLOCK:
-			
+			// copio la tarea actual del cpu
+			tarea copia;
+			copia.pid = actual_x_cpu[cpu].pid;
+			copia.bloqueado = true;
+			copia.cpu = -1;			
+			copia.quantum = 0;
+
+			// la pongo en espera
+			proc_esperando.push_back(copia);
+
+			// HACER LO MISMO QUE EN EXIT
+			// SALE FUNCION AUXILIAR (se va a llamar context switch, o switcheo de contextos^^)
+
+			break;
 		
 		case EXIT:
 			if(proc_esperando.empty())
 			{
-				actual_x_cpu[cpu] = NULL;
+				actual_x_cpu[cpu] = NULL;							//Si no tengo tareas en espera, mando la idle
 				return IDLE_TASK;
 			}else{
-				tarea copia;
+				tarea copia;										//Si tengo tareas en espera...
 				int tam = proc_esperando.size();
 				list<tarea>::iterator it = proc_esperando.begin();
 				int i;
 				
-				for(i = 0; i < tam; i++)
-				{
-					copia.pid = it->pid;
-					copia.bloqueado = it->bloqueado;
-					copia.cpu = it->cpu;
+				for(i = 0; i < tam; i++)							//Aca busco la primera tarea en espera que no
+				{													//este bloqueada, reacomodando la "cola" de
+					copia.pid = it->pid;							//manera adecuada, y se la asigno al CPU.
+					copia.bloqueado = it->bloqueado;				//ver mas abajo un caso borde, cuando no 
+					copia.cpu = it->cpu;							//hay tareas "nobloqueadas".
 					copia.quantum = it->quantum;
 					it = proc_esperando.erase(it);
 					
@@ -107,18 +139,60 @@ int SchedRR::tick(int cpu, const enum Motivo m) {
 					}
 				}
 				
-				if(i == tam)
-				{
+				if(i == tam)										//Pero, si todas estaban bloqueadas, dejo
+				{													//el mismo orden y mando la idle
 					actual_x_cpu[cpu] = NULL;
 					return IDLE_TASK;
 				}else{
-					return copia.pid;
+					return copia.pid;								//si hay nobloqueadas, mando la primera de estas
 				}
-			}
-			
-			actual_x_cpu[cpu] = ;
-		
+			}	
 	}
-	
-	
 }
+
+
+
+
+
+		// OTRA OPCION CON LOS ITERADORES DE ALE
+
+		// // ASUMO QUE EXISTE UN IT A PROC_ESPERANDO GLOBAL/ATRIBUTO (VEMOS) QUE SE LLAMA IT_SIG/IT_GLOBAL
+		// case EXIT:
+		// 	if(proc_esperando.empty())
+		// 	{
+		// 		actual_x_cpu[cpu] = NULL;							//Si no tengo tareas en espera, mando la idle
+		// 		return IDLE_TASK;
+		// 	}else{
+		// 		tarea copia;										//Si tengo tareas en espera...
+		// 		int tam = proc_esperando.size();
+		// 		list<tarea>::iterator it = proc_esperando.begin();
+		// 		int i;
+				
+		// 		for(i = 0; i < tam; i++)
+		// 		{	
+		// 			if(it_sig == proc_esperando.end())
+		// 			{
+		// 				it_sig = proc_esperando.begin();
+		// 			}
+
+		// 			if(it_sig->bloqueado)
+		// 			{
+		// 				it_sig++;
+		// 			}else{
+		// 				copia.pid = it_sig->pid;							//manera adecuada, y se la asigno al CPU.
+		// 				copia.bloqueado = it_sig->bloqueado;				//ver mas abajo un caso borde, cuando no 
+		// 				copia.cpu = cpu;									//hay tareas "nobloqueadas".
+		// 				copia.quantum = quantum_x_cpu[cpu];
+		// 				it_sig = proc_esperando.erase(it_sig);	// VER QUE PASA CUANDO SE BORRA EL DEL FINAL
+		// 				actual_x_cpu[cpu] = copia;
+		// 				break;
+		// 			}
+
+		// 		if(i == tam)										//Pero, si todas estaban bloqueadas, dejo
+		// 		{													//el mismo orden y mando la idle
+		// 			actual_x_cpu[cpu] = NULL;
+		// 			return IDLE_TASK;
+		// 		}else{
+		// 			return copia.pid;								//si hay nobloqueadas, mando la primera de estas
+		// 		}
+		// 	}
