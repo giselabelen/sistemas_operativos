cfbe90e6b315679425ebd52bc54d8e1f2baedcd6
giselabelen <gise_5291@yahoo.com.ar>
2015-10-28 12:30:23 -0300

    listo impl locks y a mitad tests

--- tp2/src/backend-multi/RWLock.cpp
+++ tp2/src/backend-multi/RWLock.cpp
@@ -1,4 +1,7 @@
 #include "RWLock.h"
+#include <cassert>
+
+#define LIMITE 1
 
 /* IMPORTANTE: Se brinda una implementación básica del Read-Write Locks
 que hace uso de la implementación provista por pthreads. Está dada para
@@ -10,25 +13,71 @@ Condición. */
 
 RWLock :: RWLock() {
     /* Cambiar por su implementación */
-    pthread_rwlock_init(&(this->rwlock),NULL);
+    //~ pthread_rwlock_init(&(this->rwlock),NULL);
+    pthread_mutex_init(&mutex,NULL);
+	lectores = 0;
+	escritores = 0;
+	leyendo = 0;
+	escribiendo = 0;
+	contador = 0;
+	cont2 = 0;
+	pthread_cond_init(&cond_esperoleer,NULL);
+	pthread_cond_init(&cond_esperoescribir,NULL);
 }
 
 void RWLock :: rlock() {
     /* Cambiar por su implementación */
-    pthread_rwlock_rdlock(&(this->rwlock));
+    //~ pthread_rwlock_rdlock(&(this->rwlock));
+    pthread_mutex_lock(&mutex);
+	lectores++;
+	while(escritores > 0 && contador <= LIMITE){
+		pthread_cond_wait(&cond_esperoleer,&mutex);
+	}
+	leyendo++;
+	cont2--;
+	if(cont2 == 0){ contador = 0; }
+	pthread_mutex_unlock(&mutex);
 }
 
 void RWLock :: wlock() {
     /* Cambiar por su implementación */
-    pthread_rwlock_wrlock(&(this->rwlock));
+    //~ pthread_rwlock_wrlock(&(this->rwlock));
+    pthread_mutex_lock(&mutex);
+	escritores++;
+	while(leyendo > 0 || escribiendo > 0 || (lectores > 0 && contador >= LIMITE)){
+		pthread_cond_wait(&cond_esperoescribir,&mutex);
+	}
+	escribiendo++;
+	assert(escribiendo == 1);
+	contador++;
+    pthread_mutex_unlock(&mutex);
 }
 
 void RWLock :: runlock() {
     /* Cambiar por su implementación */
-    pthread_rwlock_unlock(&(this->rwlock));
+    //~ pthread_rwlock_unlock(&(this->rwlock));
+    pthread_mutex_lock(&mutex);
+    lectores--;
+	leyendo--;
+	assert(lectores >= 0 && leyendo >= 0);
+	if(lectores == 0){
+		pthread_cond_signal(&cond_esperoescribir);
+	}
+    pthread_mutex_unlock(&mutex);
 }
 
 void RWLock :: wunlock() {
     /* Cambiar por su implementación */
-    pthread_rwlock_unlock(&(this->rwlock));
+    //~ pthread_rwlock_unlock(&(this->rwlock));
+    pthread_mutex_lock(&mutex);
+    escritores--;
+	escribiendo--;
+	assert(escritores >= 0 && escribiendo == 0);
+	if(escritores == 0 || contador >= LIMITE){
+		cont2 = lectores;
+		pthread_cond_broadcast(&cond_esperoleer);
+	}else{
+		pthread_cond_signal(&cond_esperoescribir);
+    }
+    pthread_mutex_unlock(&mutex);
 }
--- tp2/src/backend-multi/RWLockTest.cpp
+++ tp2/src/backend-multi/RWLockTest.cpp
@@ -1,10 +1,48 @@
 #include "RWLock.h"
+#include <cstdio>
 
-int main(int argc, char* argv[]) {
+#define CANT_ESCRITORES	1
+#define CANT_LECTORES	50
+#define CANT_THREADS	101
 
-    /* Implementar */
+RWLock lock;
+
+void *escritor(void *p_minumero)
+{
+	lock.wlock();
+	int minumero = *((int *) p_minumero);
+	printf("Escribiendo. Soy el thread nro. %d.\n", minumero);
+	lock.wunlock();
+	return NULL;
+}
+
+void *lector(void *p_minumero)
+{
+	lock.rlock();
+	int minumero = *((int *) p_minumero);
+	printf("Leyendo. Soy el thread nro. %d.\n", minumero);
+	lock.runlock();
+	return NULL;
+}
+
+int main(int argc, char* argv[]) 
+{
+    pthread_t thread[CANT_THREADS];
+    int nros[CANT_THREADS];
+    int tid;
+    
+	for (tid = 0; tid < CANT_THREADS; tid++){
+		nros[tid]=tid;
+		if (tid == CANT_LECTORES){
+			pthread_create(&thread[tid], NULL, escritor, &nros[tid]);
+		}else{
+			pthread_create(&thread[tid], NULL, lector, &nros[tid]);
+		}
+	}
+	
+	for (tid = 0; tid < CANT_THREADS; tid++){
+		pthread_join(thread[tid], NULL);
+	}
 
     return 0;
 }
-
-
